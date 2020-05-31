acd3a9154b09d8311d425e3df7aa3d2894bd9fe6
pral36 <56882382+pral36@users.noreply.github.com>
2020-05-31 21:44:37 +0800

    Added facebook-login js file

---
+++ src/features/login/facebook-login.js
@@ -0,0 +1,36 @@
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '955941594865244',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v7.0'
+  });
+    
+  FB.AppEvents.logPageView();   
+    
+};
+
+
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+ function statusChangeCallback(response){
+     if(response.status="connected"){
+         console.log("Logged in and authenticated");
+     }
+     else{
+         console.log("Not authenticated");
+     }
+ };
+
+ 
