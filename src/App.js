e217a493849f1b7d5b0daa0174b05a66fd87d713
Kamal Kamal Azmi <kamal.kamalazmi@gmail.com>
2020-05-31 17:59:04 +0800

    Added material-ui and SignIn component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import shoppingCart from './shopping-cart.svg';
 import './App.css';
 
 import { GoogleLogin } from './features/login/Login'
+import SignIn from './features/login/SignIn';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={shoppingCart} className="App-logo" alt="logo" />
         <GoogleLogin/>
+        <SignIn/>
       </header>
     </div>
   );
