d55c55c4866daa5cb3a6031ad91cdb343c14e958
Kamal Kamal Azmi <kamal.kamalazmi@gmail.com>
2020-05-31 19:09:53 +0800

    Updated package.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import shoppingCart from './shopping-cart.svg';
 import './App.css';
-
 import { GoogleLogin } from './features/login/Login'
 import SignIn from './features/login/SignIn';
+
 function App() {
   return (
     <div className="App">
