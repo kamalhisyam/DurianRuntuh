e780074e7a0366dc4bd290acfa03bfc789ae95ed
Kamal Kamal Azmi <kamal.kamalazmi@gmail.com>
2020-06-03 22:14:05 +0800

    Added pages Home and Login

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import shoppingCart from './shopping-cart.svg';
+import './App.css';
+import { GoogleLogin } from './features/login/Login'
+import SignIn from './features/login/SignIn';
+import { Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={shoppingCart} className="App-logo" alt="logo" />
+    //     <GoogleLogin/>
+    //     <SignIn/>
+    //   </header>
+    // </div>
+    
+  );
+}
+
+export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Home () {
+export default function Login () {
     return (
     <div>
-        Home!
+        Login!
     </div>
     )
 }
