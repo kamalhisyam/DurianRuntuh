c4095e1c546dce0f73c39d00ead80144a2854120
Kamal Kamal Azmi <kamal.kamalazmi@gmail.com>
2020-06-04 21:07:43 +0800

    Minor edits to remove comments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,36 +6,44 @@ import ProductCard from '../components/ProductCard';
 export default function Home() {
     return (
         <Grid container>
-            <Grid item xs={12} sm={4}>
-                <ProductCard    productName='Sony Headset' 
-                                productDescription='High quality Sony headset for cheap'
-                                price='399.00'
-                                rallyCount='379'
-                                minCount='1000' 
-                />
+            <Grid item sm={2} />
+            <Grid item container sm={8}>
+                <Grid item xs={12} sm={4}>
+                    <ProductCard
+                        productName='Sony Headset'
+                        productDescription='High quality Sony headset for cheap'
+                        price='399.00'
+                        rallyCount='379'
+                        minCount='1000'
+                    />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <ProductCard
+                        productName='Sony PlayStation 5'
+                        productDescription='State of the art gaming console'
+                        price='3,499.00'
+                        rallyCount='5,687'
+                        minCount='10,000'
+                    />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <ProductCard
+                        productName='Intel i5-9400H'
+                        productDescription='High quality Sony headset for cheap'
+                        price='499.00'
+                        rallyCount='578'
+                        minCount='1000'
+                    />
+                </Grid>
+                <Button
+                    variant='contained'
+                    href='/Login'
+                >
+                    Back to Login Page
+                </Button>
             </Grid>
-            <Grid item xs={12} sm={4}>
-            <ProductCard    productName='Sony PlayStation 5' 
-                                productDescription='State of the art gaming console'
-                                price='3,499.00'
-                                rallyCount='5,687'
-                                minCount='10,000' 
-                />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-            <ProductCard    productName='Intel i5-9400H' 
-                                productDescription='High quality Sony headset for cheap'
-                                price='499.00'
-                                rallyCount='578'
-                                minCount='1000' 
-                />
-            </Grid>
-            <Button
-                variant='contained'
-                href='/Login'
-            >
-                Back to Login Page
-        </Button>
+            <Grid item sm={2} />
+
         </Grid>
 
     )
